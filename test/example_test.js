ff76ca2ad1b641c44aec4506f7f218cb117e1f43
Roman <R.novosad@outlook.com>
2016-12-23 21:02:06 +0200

    Update example_test.js

--- test/example_test.js
+++ test/example_test.js
@@ -3,9 +3,7 @@ var assert = require('assert')
 var should = require('should');
 
 var branchName = process.env.TRAVIS_BRANCH
-    var branchUrl = branchName
-        ? 'http://travis-ci-rnovosad-' + branchName + '.herokuapp.com'
-        : 'http://myurl.com'
+var branchUrl = 'http://travis-ci-rnovosad-' + branchName + '.herokuapp.com'
     
 describe('Home page tests', function() {
     it('Header is correct', function() {
